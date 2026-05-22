c70a1998c9614c1622a0fd58cd7111e0e04f4b7e
Samuel Díaz <sammu.diazs@gmail.com>
2026-05-22 21:12:25 +0000

    login

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -8,9 +9,21 @@ templates = Jinja2Templates(directory="templates")
 async def inicio(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
-@app.post("login")
-async def login(request: Request):
+
+
+@app.post("/enviar")
+async def enviar_mensaje(usuario: str = Form(...), contraseña: str = Form(...)):
+    usuario_correcto = "jhon"
+    contraseña_correcto = "1234" 
     
+    if usuario == usuario_correcto and contraseña == contraseña_correcto:
+        return RedirectResponse(url="/bienvenido", status_code=303)
+    else:
+        return RedirectResponse(url="/", status_code=303)
     
+@app.get("/bienvenido")
+async def inicio(request: Request):
+    return templates.TemplateResponse(request=request, name="bienvenido.html")
+
 
 
